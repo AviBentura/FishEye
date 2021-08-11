5bbf5d7010b6bd5517575c61663af02822184a4b
Bentura <avi.bentura@gmail.com>
2021-08-11 13:19:27 +0200

    Js - mise à jour

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 const containePost = document.querySelector(".content");
 
+const create = () => {
+  if (photographer.tags == undefined) {
+    console.log("n");
+  }
+};
+
 fetch("data.json")
   .then((res) => res.json())
   .then((data) =>
     data.photographers.forEach((photographer) => {
-      let newContent = document.createElement("div");
-      //let newFigure = document.querySelectorAll(".boxContainer");
-      newContent.classList += "artist";
-      newContent.innerHTML +=
-        '<figure class="boxContainer"><img class="boxImage" src="./FishEye_Photos/Photos/' +
-        photographer.dossier +
-        "/" +
-        photographer.portrait +
-        '"/></figure>';
-      console.log(newContent);
       containePost.innerHTML +=
         '<div class="artist"><figure class="boxContainer"><img class="boxImage" src="./FishEye_Photos/Photos/' +
         photographer.dossier +
         "/" +
         photographer.portrait +
-        '"/></figure></div>';
-      /*
-      '<img src="./FishEye_Photos/Photos/' +
-        photographer.dossier +
-        "/" +
-        photographer.portrait +
-        '"></img>';*/
+        '"/></figure><figcaption>' +
+        photographer.name +
+        "</figcaption><h3>" +
+        photographer.city +
+        ", " +
+        photographer.country +
+        "</h3><p>" +
+        photographer.tagline +
+        "</p><h4>" +
+        photographer.price +
+        '€/jour</h4><button class="styleBouton">#' +
+        photographer.tags[0] +
+        '</button><button class="styleBouton">#' +
+        photographer.tags[1] +
+        '</button><button class="styleBouton">#' +
+        photographer.tags[2] +
+        "</button></div>";
     })
   );
 
