22913588447af4fa549fe89a2e9489e14dc340d1
Bentura <avi.bentura@gmail.com>
2021-08-15 11:30:19 +0200

    JS - Mise à jour Code

--- dataFunction.js
+++ dataFunction.js
@@ -1,3 +1,19 @@
 fetch("data.json")
   .then((res) => res.json())
   .then((data) => console.log(data));
+data.photographers.forEach((photographer) => {
+  containePost.innerHTML += `<div class="artist"><figure class="boxContainer"><img class="boxImage" src="./FishEye_Photos/Photos/Photographers ID Photos/${photographer.portrait}"/></figure><figcaption>${photographer.name}</figcaption><h3>${photographer.city}, ${photographer.country}</h3><p>${photographer.tagline}</p><h4>${photographer.price}€/jour</h4>`;
+  function getAllTags(photographers) {
+    let tags = [];
+  }
+  console.log("all tags", tags);
+  document.querySelector(".flexible");
+  console.log("all tags", tags);
+  /*<button class="styleBouton">#' +
+        photographer.tags[0] +
+        '</button><button class="styleBouton">#' +
+        photographer.tags[1] +
+        '</button><button class="styleBouton">#' +
+        photographer.tags[2] +
+        "</button></div>";*/
+});
--- script.js
+++ script.js
@@ -1,39 +1,25 @@
 const containePost = document.querySelector(".content");
 
-const create = () => {
-  if (photographer.tags == undefined) {
-    console.log("n");
-  }
-};
-
 fetch("data.json")
   .then((res) => res.json())
   .then((data) =>
+    /*function getAllTags(photographers) {
+        let tags = [];
+    }
+    console.log("all tags", tags);
+    /*document.querySelector('.flexible');
+    console.log("all tags", tags);
     data.photographers.forEach((photographer) => {
-      containePost.innerHTML +=
-        '<div class="artist"><figure class="boxContainer"><img class="boxImage" src="./FishEye_Photos/Photos/' +
-        photographer.dossier +
-        "/" +
-        photographer.portrait +
-        '"/></figure><figcaption>' +
-        photographer.name +
-        "</figcaption><h3>" +
-        photographer.city +
-        ", " +
-        photographer.country +
-        "</h3><p>" +
-        photographer.tagline +
-        "</p><h4>" +
-        photographer.price +
-        '€/jour</h4><button class="styleBouton">#' +
+      containePost.innerHTML += `<div class="artist"><figure class="boxContainer"><img class="boxImage" src="./FishEye_Photos/Photos/Photographers ID Photos/${photographer.portrait}"/></figure><figcaption>${photographer.name}</figcaption><h3>${photographer.city}, ${photographer.country}</h3><p>${photographer.tagline}</p><h4>${photographer.price}€/jour</h4>`;
+      /*<button class="styleBouton">#' +
         photographer.tags[0] +
         '</button><button class="styleBouton">#' +
         photographer.tags[1] +
         '</button><button class="styleBouton">#' +
         photographer.tags[2] +
         "</button></div>";
-    })
-  );
+    })*/
+  )
 
 //let newContent = document.createElement("div");
 //newContent.classList = "artist";
