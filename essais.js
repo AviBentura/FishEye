52a42bfe4d0bb7ca5254431f54f68ce48a8a3568
Bentura <avi.bentura@gmail.com>
2021-08-15 16:25:02 +0200

    JS - Création des Tags de recherches

---
+++ essais.js
@@ -0,0 +1,32 @@
+/*function getAllTags(photographers) {
+  let tags = [];
+  photographers.forEach((photographer) => {
+    tags = tags.concat(photographer.tags);
+  });
+  console.log("tags", tags);
+  let tagSet = new Set(tags);
+  return Array.from(new Set(tags));
+}
+
+
+fetch("data.json")
+   .then((res) => res.json());
+   .then((data) => {
+       let tags = getAllTags(data.photographers);
+       document.querySelector('.flexible').innerHTML = tags.map(tag => `<button class="styleBouton">#${tag}</button>`);
+       console.log("all tags", tags);
+       data.photographers.forEach((photographer) => {
+      containePost.innerHTML += `<div class="artist"><figure class="boxContainer"><img class="boxImage" src="./FishEye_Photos/Photos/Photographers ID Photos/${photographer.portrait}"/></figure><figcaption>${photographer.name}</figcaption><h3>${photographer.city}, ${photographer.country}</h3><p>${photographer.tagline}</p><h4>${photographer.price}€/jour</h4>`;
+       })
+   })
+
+fetch("data.json")
+   .then((res) => res.json());
+   .then((data) => {
+       data.photographers.forEach((photographer) => {
+      containePost.innerHTML += `<div class="artist"><figure class="boxContainer"><img class="boxImage" src="./FishEye_Photos/Photos/Photographers ID Photos/${photographer.portrait}"/></figure><figcaption>${photographer.name}</figcaption><h3>${photographer.city}, ${photographer.country}</h3><p>${photographer.tagline}</p><h4>${photographer.price}€/jour</h4>`;
+      let tags = getAllTags(tags.photographers);
+       document.querySelector('.flexible').innerHTML = tags.map(tag => `<button class="styleBouton">#${tag}</button>`);
+       console.log("all tags", tags);
+       })
+   })*/
--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 const containePost = document.querySelector(".content");
 
+function getAllTags(photographers) {
+  let tags = [];
+  photographers.forEach((photographer) => {
+    tags = tags.concat(photographer.tags);
+  });
+  console.log("tags", tags);
+  let tagSet = new Set(tags);
+  return Array.from(new Set(tags));
+}
+
 fetch("data.json")
   .then((res) => res.json())
-  .then((data) =>
-    /*function getAllTags(photographers) {
-        let tags = [];
-    }
-    console.log("all tags", tags);
-    /*document.querySelector('.flexible');
-    console.log("all tags", tags);
+  .then((data) => {
     data.photographers.forEach((photographer) => {
       containePost.innerHTML += `<div class="artist"><figure class="boxContainer"><img class="boxImage" src="./FishEye_Photos/Photos/Photographers ID Photos/${photographer.portrait}"/></figure><figcaption>${photographer.name}</figcaption><h3>${photographer.city}, ${photographer.country}</h3><p>${photographer.tagline}</p><h4>${photographer.price}€/jour</h4>`;
-      /*<button class="styleBouton">#' +
-        photographer.tags[0] +
-        '</button><button class="styleBouton">#' +
-        photographer.tags[1] +
-        '</button><button class="styleBouton">#' +
-        photographer.tags[2] +
-        "</button></div>";
-    })*/
-  )
+      let tags = getAllTags(data.photographers);
+      document.querySelector(".flexible").innerHTML = tags.map(
+        (tag) => `<button class="styleBouton">#${tag}</button>`
+      );
+    });
+  });
 
 //let newContent = document.createElement("div");
 //newContent.classList = "artist";
